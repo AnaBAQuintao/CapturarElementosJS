b0607f277c10e928fca83c6522e32e5c9cf3d3ef
Ana Quintao <ana.quintao@localiza.com>
2024-07-15 21:10:40 -0300

    Capturar elementos no JS

---
+++ style.js
@@ -0,0 +1,20 @@
+console.log("Deu certo")
+
+const formularioId = document.getElementById("formulario");
+console.log("formularioId")
+ 
+const dataClass = document.getElementsByClassName("post-data");
+console.log("dataClass")
+ 
+const textClass = document.getElementsByClassName("post-texto");
+console.log("textClass")
+ 
+const listaClass = document.getElementsByClassName("lista_redes");
+console.log("listaClass")
+
+const elementoPorQuery = document.querySelector("post01 .post-texto");
+console.log('elementoPorQuery');
+ 
+const elementoPorQueryAll = document.querySelectorAll("post02 .strong");
+console.log('elementoPorQueryAll');
+
